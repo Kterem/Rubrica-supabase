d11421f42303be680922f375486ad1a6e5a5028c
Kterem <danielerollin@gmail.com>
2025-05-16 14:27:23 +0200

    Create rubrica_script.sh

---
+++ rubrica_script.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Versione iniziale rubrica con Supabase"
+git branch -M main
+git remote add origin https://github.com/TUO_USERNAME/NOME_REPOSITORY.git # Sostituisci con il tuo URL
+git push -u origin main
