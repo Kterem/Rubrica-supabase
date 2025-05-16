8717d1b10ebf3726ae9a428b258e374d609d7ada
Kterem <danielerollin@gmail.com>
2025-05-16 14:32:02 +0200

    Update rubrica_script.sh

--- rubrica_script.sh
+++ rubrica_script.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Versione iniziale rubrica con Supabase"
 git branch -M main
-git remote add origin https://github.com/TUO_USERNAME/NOME_REPOSITORY.git # Sostituisci con il tuo URL
+git remote add origin https://github.com/Kterem/Rubrica-supabase.git # Sostituisci con il tuo URL
 git push -u origin main
